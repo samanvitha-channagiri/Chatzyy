9342f3b979ae22a6bb2df6a5c179ec4c929bb588
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-04-16 19:26:36 +0530

    setting page for theme selection

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand";
 
 export const useThemeStore=create((set)=>(
     {
-        theme:localStorage.getItem("chat-theme")||"light",
+        theme:localStorage.getItem("chat-theme")||"retro",
         setTheme:(theme)=>{
             localStorage.setItem("chat-theme",theme);
             set({theme});

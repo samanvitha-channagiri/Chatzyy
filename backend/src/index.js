f7c199d809bd957cef55e640ee7881b66258e0a2
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-04-17 16:23:10 +0530

    regex update

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,12 +25,24 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/settings", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
   
 
 
